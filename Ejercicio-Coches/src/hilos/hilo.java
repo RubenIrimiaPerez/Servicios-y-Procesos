94f3745bd88bea93fc49ed98505ae64f1e697d21
RubenIrimiaPerez <rubenbaloncesto10@gmail.com>
2023-10-18 14:30:18 +0200

    constructor

--- Ejercicio-Coches/src/hilos/hilo.java
+++ Ejercicio-Coches/src/hilos/hilo.java
@@ -1,5 +1,32 @@
 package hilos;
 
-public class hilo {
+public class Hilo {
+	private String Nombre;
+	private int Vuelta;
+	
+	public Hilo(String nombre, int vuelta) {
+		Nombre = nombre;
+		Vuelta = vuelta;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public int getVuelta() {
+		return Vuelta;
+	}
+	public void setVuelta(int vuelta) {
+		Vuelta = vuelta;
+	}
+	
+	public void run() {
+		
+	}
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- Ejercicio-Coches/src/main/Main.java
+++ Ejercicio-Coches/src/main/Main.java
@@ -1,9 +1,20 @@
 package main;
 
+import java.util.Scanner;
+
+import hilos.Hilo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+
+		Hilo h1 = new Hilo("Coche1",1);
+		Hilo h2 = new Hilo("Coche2",1);
+
+		h1.start();
+		h2.start();
 
 	}
 
