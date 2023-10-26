8fe9fc70c84ee229f1f39b3f9919f1425ed0115c
RubenIrimiaPerez <rubenbaloncesto10@gmail.com>
2023-10-26 11:55:04 +0200

    metodo ordenarCoches

--- Ejercicio-Coches/src/Thread/Coche.java
+++ Ejercicio-Coches/src/Thread/Coche.java
@@ -3,40 +3,37 @@ package Thread;
 import java.util.Random;
 
 public class Coche extends Thread {
-	 private String nombre;
-     private int vueltas;
 
-     public Coche(String nombre) {
-         this.nombre = nombre;
-         this.vueltas = 0;
-     }
+	private String nombre;
+	private int vueltas;
 
-     public String getNombre() {
-         return nombre;
-     }
+	public Coche(String nombre) {
+		this.nombre = nombre;
+		this.vueltas = 0;
+	}
 
-     public int getVueltas() {
-         return vueltas;
-     }
-     
-   //Método run se simula la vuelta de un coche. 
-   //Se genera un tiempo de vuelta aleatorio entre 0.5 y 3 segundos con un Random.
-     public void run() {
-         Random random = new Random();
-         for (int vueltas = 1; vueltas <= 10; vueltas++) {
-             int VueltaSegundos = random.nextInt(500,3000); // Tiempo aleatorio entre 0.5 y 3 segundos
-             try {
-                 Thread.sleep(VueltaSegundos);// se duerme el hilo para simular lo que tarda en la vuelta si el random es 2000 ms se duerme eso tiempo simulando la vuelta
-             } catch (InterruptedException e) {
-                 e.printStackTrace();
-             }
-             vueltas++;
-         }
-     }
- }
+	public String getNombre() {
+		return nombre;
+	}
 
+	public int getVueltas() {
+		return vueltas;
+	}
 
+	@Override
+	public void run() {
+		Random random = new Random();
+		for (int vuelta = 1; vuelta <= 10; vuelta++) {
+			try {
+				int tiempoVuelta = random.nextInt(500,3000); // Tiempo aleatorio entre 0.5 y 3 segundos 
+				vueltas++;
+				Thread.sleep(tiempoVuelta);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 
+		}
+	}
+	
 
-
-
+}
--- Ejercicio-Coches/src/main/Main.java
+++ Ejercicio-Coches/src/main/Main.java
@@ -1,36 +1,61 @@
 package main;
 
-
-
 import Thread.Coche;
 
 public class Main {
 
 	public static void main(String[] args) {
+		int CantidadCoche = 10; // Cantidad de pilotos en la carrera
+		int CochesCompletados =0;
+		Coche[] coches = new Coche[CantidadCoche];
 
-		int NumeroCoches = 10;
-		Coche[] coches =new Coche[NumeroCoches];
-		 //se crean los 10 coches
-		for(int i = 0; i<NumeroCoches; i++) {
-			coches[i]= new Coche("Coche" + (i+1));
+		// Inicializar los coches y comenzar la carrera
+		for (int i = 0; i < CantidadCoche; i++) {
+			coches[i] = new Coche("Coche" + (i + 1));
 			coches[i].start();
 		}
-		 System.out.println("427527");
-		
-		 for (int vueltas = 1; vueltas <= 10; vueltas++) {
+		do {
+			// Simular la carrera durante 10 vueltas
+			for (int vuelta = 0; vuelta <= 10; vuelta++) {
 				try {
-					Thread.sleep(1000);
+					Thread.sleep(1000); // Esperar 1 segundo entre actualizaciones
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-		 }
+				
+				// Ordenar los coches según las vueltas completadas y mostrar el estado
+				ordenarCoches(coches);
+				System.out.println("Coche   Vuelta     Ranking");
+				System.out.println("****************************");
+				for (int i = 0; i < CantidadCoche; i++) {
+					Coche coche = coches[i];
+					System.out.println(coche.getNombre() + "       " + coche.getVueltas() + "        " + (i + 1));
+				}
+				System.out.println("\n");
+				
+				
+			}
+			CochesCompletados++;
 		
-		
-    System.out.println("hsgfuiefuiheaufhbj");
-
+		}while(CochesCompletados<10);
 		
 
 	}
 
-}
+	private static void ordenarCoches(Coche[] coches) {
+		for (int i = 0; i < coches.length - 1; i++) {
+			for (int j = 0; j < coches.length - i - 1; j++) {
+				if (coches[j].getVueltas() < coches[j + 1].getVueltas()) {
+					Coche temp = coches[j];
+					coches[j] = coches[j + 1];
+					coches[j + 1] = temp;
+				}
+			}
+		}
+	}	
+	
+		
+	}
+
+
+
